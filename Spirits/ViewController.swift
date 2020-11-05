c32f4b910dfa8d8acad5a0ebbfd65f29d873f45c
SchmuW <sammy@jwaterston.com>
2020-11-05 14:54:53 -0500

    Initial Commit

---
+++ Spirits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spirits
+//
+//  Created by  on 11/4/20.
+//  Copyright © 2020 Owen Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var AaLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AaLabel.font = UIFont(name: "LadyRadical", size: 30)
+    }
+
+
+}
+
